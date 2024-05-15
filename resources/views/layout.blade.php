e7295bbd5b4ed4d000461ffd29d615d22712ebbb
unknown <peterjeronimojr@gmail.com>
2024-05-15 10:04:42 +0800

    First Commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1400px">
+
+<!-- Header -->
+<header class="w3-container w3-center w3-padding-32"> 
+  <h1><b>MY PORTFOLIO</b></h1>
+  <p>Welcome to the LIFE OF <span class="w3-tag">PETER</span></p>
+</header>
+
+<!-- Grid -->
+<div class="w3-row">
+
+<!-- Blog entries -->
+<div class="w3-col l8 s12">
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+    <img  scr = "{{ URL('images/peter.jpg') }}" alt="Peter IMG" style="width:100%">
+    <div class="w3-container">
+      <h3><b>EDUCATIONAL ATTAINMENT</b></h3>
+      <h5>ELEMENTARY, <span class="w3-opacity">YEAR: 2007</span></h5>
+    </div>
+
+    <div class="w3-container">
+      <p>I am Graduated at Bato Elementary School.</p>
+    </div>
+
+    <div class="w3-container">
+      <h5>HighSchool, <span class="w3-opacity">YEAR: 2011</span></h5>
+    </div>
+
+    <div class="w3-container">
+      <p>I am Graduated at Naguilian National Highschool.</p>
+    </div>
+
+    <div class="w3-container">
+      <h5>College, <span class="w3-opacity">YEAR: 2016</span></h5>
+    </div>
+
+    <div class="w3-container">
+      <p>I am Graduated at Don Mariano Marcos Memorial State University.</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+        </div>
+        <div class="w3-col m4 w3-hide-small">
+          <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-tag">0</span></span></p>
+        </div>
+      </div>
+    </div>
+
+    
+  </div>
+  <hr>
+
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="/w3images/bridge.jpg" alt="Norway" style="width:100%">
+    <div class="w3-container">
+      <h3><b>What Language I love to code</b></h3>
+      <h5>What I love about Programming, </h5>
+    </div>
+
+    <div class="w3-container">
+      <p>A dedicated programmer with a deep love for coding and an insatiable curiosity for exploring diverse programming languages. With expertise spanning from the simplicity of Python to the intricacies of C++, I enjoy solving complex problems and building innovative solutions. Whether it’s web development with JavaScript, data analysis with R, or creating efficient algorithms in Java, I thrive on the challenge of learning and mastering new languages. Join me on this coding journey as I share tips, tutorials, and insights from the world of software development, inspiring others to turn ideas into reality through code.</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+        </div>
+        <div class="w3-col m4 w3-hide-small">
+          <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-badge">2</span></span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- Introduction menu -->
+<div class="w3-col l4">
+  <!-- About Card -->
+  <div class="w3-card w3-margin w3-margin-top">
+  <img src="/w3images/avatar_g.jpg" style="width:100%">
+    <div class="w3-container w3-white">
+      <h4><b>Peter Jeronimo Rimando Jr</b></h4>
+      <p>Passionate vlogger capturing the essence of everyday adventures and extraordinary journeys. With a camera in hand and an eye for storytelling, I turn moments into memories, exploring the world one vlog at a time. From travel escapades to daily life snippets, join me as I share my unique perspective, authentic experiences, and creative insights. Subscribe for a blend of inspiration, entertainment, and a glimpse into the life behind the lens. Let's embark on this visual journey together!</p>
+    </div>
+  </div><hr>
+  
+  <!-- Posts -->
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Hobby</h4>
+    </div>
+    <ul class="w3-ul w3-hoverable w3-white">
+      <li class="w3-padding-16">
+        <img src="/w3images/workshop.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">Programming</span><br>
+        <span>PHP,JavaScript</span>
+      </li>
+      <li class="w3-padding-16">
+        <img src="/w3images/gondol.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">Vlogging</span><br>
+        <span>I am Adventurist</span>
+      </li> 
+      <li class="w3-padding-16">
+        <img src="/w3images/skies.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">Playing Video Games</span><br>
+        <span>ML</span>
+      </li>   
+      <li class="w3-padding-16 w3-hide-medium w3-hide-small">
+        <img src="/w3images/rock.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">Riding a Motor Cycle</span><br>
+        <span></span>
+      </li>  
+    </ul>
+  </div>
+  <hr> 
+ 
+  <!-- Labels / tags -->
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Tags</h4>
+    </div>
+    <div class="w3-container w3-white">
+    <p><span class="w3-tag w3-black w3-margin-bottom">Travel</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">New York</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">London</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">IKEA</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">NORWAY</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">DIY</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Ideas</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Baby</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Family</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">News</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Clothing</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Shopping</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Sports</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Games</span>
+    </p>
+    </div>
+  </div>
+  
+<!-- END Introduction Menu -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+
+<!-- END w3-content -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
+  <button class="w3-button w3-black w3-disabled w3-padding-large w3-margin-bottom">Previous</button>
+  <button class="w3-button w3-black w3-padding-large w3-margin-bottom">Next »</button>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+
+</body>
+</html>
